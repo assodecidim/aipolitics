a156f4a85d2fe5e59fc1c4006b738c33f582cab0
Andrés Pereira de Lucena <andreslucena@gmail.com>
2024-05-07 13:22:48 +0200

    Check if the SENTRY_DSN env var is set before using it

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
-Sentry.init do |config|
-  config.dsn = ENV.fetch("SENTRY_DSN")
-  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+if ENV.has_key?("SENTRY_DSN")
+  Sentry.init do |config|
+    config.dsn = ENV.fetch("SENTRY_DSN")
+    config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+  end
 end
